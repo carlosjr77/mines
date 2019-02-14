5678b8d444efb24c09c2e8968c54900d45915ffa
carlosconde_ <carlos.conde@radixeng.com.br>
2019-02-14 15:46:31 -0200

    criação do field do campos

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import params from './src/params'
+import Field from './src/components/Field'
 
 export default class App extends Component {
   render() {
@@ -8,6 +9,12 @@ export default class App extends Component {
       <View style={styles.container}>
         <Text style={styles.welcome}>Iniciando o Mines!</Text>
         <Text style={styles.instructions}>Tamanho da grade: {params.getRowsAmount()}*{params.getColumnsAmount()}</Text>
+        <Field />
+        <Field opened/>
+        <Field opened nearMines={1}/>
+        <Field opened nearMines={2}/>
+        <Field opened nearMines={3}/>
+        <Field opened nearMines={6}/>
       </View>
     );
   }
